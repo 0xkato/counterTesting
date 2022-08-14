59476cb24a400c0f7458c3ea614833300cc21288
0xKato <emtolihi@gmail.com>
2022-08-14 12:15:01 +0200

    update

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,13 +2,43 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
+    uint public number;
+    string public name;
+
+    // initiate contract with a name and a starting number
+    constructor(string memory _name, uint _initialNumber) {
+        name = _name;
+        number = _initialNumber;
+    }
+
+    // Sets the change the number of the count
+    function changeNumber(uint newNumber) public {
         number = newNumber;
     }
 
-    function increment() public {
+    //change the name of the name variable
+    function changeName(string memory newName) public {
+        name = newName;
+    }
+
+    // increment the count by 1 everytime its called
+    function increment() public returns (uint) {
         number++;
+        return number;
+    }
+
+    // decrement the count by 1 everytime its called if number is > then 0
+    function decrement() public returns (uint) {
+        if (number > 0) {
+        number--;
+        }
+        return number;
+    }
+
+    // resets the count everytime its called and makes the number go to 0
+    function  resetCount() public returns (uint newCount) {
+        number = 0;
+        return number;
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -6,9 +6,11 @@ import "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+
     function setUp() public {
-       counter = new Counter();
-       counter.setNumber(0);
+       counter = new Counter("start", 5);
+       counter.changeNumber(0);
+       counter.changeName("First");
     }
 
     function testIncrement() public {
@@ -16,8 +18,21 @@ contract CounterTest is Test {
         assertEq(counter.number(), 1);
     }
 
+    function testDecrement() public {
+        counter.changeNumber(100);
+        counter.decrement();
+        assertEq(counter.number(), 99);
+    }
+
     function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
+        counter.changeNumber(x);
         assertEq(counter.number(), x);
     }
+
+    function testResetCounter(uint256 x) public {
+        counter.changeNumber(x);
+        counter.resetCount();
+        assertEq(counter.number(), 0);
+    }
+
 }
